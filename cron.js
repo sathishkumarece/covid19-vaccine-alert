7d5bb41d05599b2b4beba493a425039ad3529b95
Sathishkumar NATARAJ <sathishkumarece@gmail.com>
2021-05-23 00:17:12 +0530

    Invoking alert sending process

--- cron.js
+++ cron.js
@@ -2,9 +2,9 @@
 const cron = require('node-cron');
 const autoTiggerService = require('./service/autoTriggerService')
 
-const task = cron.schedule('6,9,12,15,18 * * * *', () => {
-    logger.info('cronjob started...');
-    // autoTiggerService.triggerAlert(hour);
+const task = cron.schedule('* 6,9,12,15,18 * * *', () => {
+    console.log('cronjob started...');
+    autoTiggerService.triggerAlertByHour(new Date().getHours());
 }, {
     scheduled: false,
     timezone: "Asia/kolkata"
