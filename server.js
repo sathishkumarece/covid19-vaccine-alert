7074969fbcf654373522f50d1b050bd4a86b55be
Sathishkumar NATARAJ <sathishkumarece@gmail.com>
2021-05-24 19:50:25 +0530

    Feat: Rate limit to limit number of api calls

--- server.js
+++ server.js
@@ -5,11 +5,24 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
+const rateLimit = require('express-rate-limit');
 const routes = require('./routes');
 const client = require('./service/whatsappService');
 
 //Trigger cron job
 require('./cron');
+
+//Limit the number of alert created
+const apiLimiter = rateLimit({
+    windowMs: 60 * 60 * 1000, // 1 hour window
+    max: 5, // start blocking after 5 requests
+    message:  {
+          status: false,
+          code: 429,
+          data :{},
+          message:"Too many request from this IP, allow limit: 5 per hour. please try again after an hour"
+        }
+  });
 
 const app = express();
 
@@ -18,6 +31,8 @@ app.use(helmet());
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({'extended':true}));
+app.set('trust proxy', 1);
+app.use(apiLimiter);
 
 dotenv.config();
 
@@ -31,7 +46,7 @@ if(process.env.NODE_ENV ==='production'){
 }
 
 app.get('/wsm', async (req,res)=>{
-    await client.sendMessage("917604961875" + '@c.us', "Greeting!")
+    // await client.sendMessage("917604961875" + '@c.us', "Greeting!")
     res.send('Message sent');
 })
 
