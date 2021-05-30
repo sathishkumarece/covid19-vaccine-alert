8529c1eae1c95b0d0335bccc3b2b5260d7297233
Sathishkumar NATARAJ <sathishkumarece@gmail.com>
2021-05-30 10:02:37 +0530

    Bug: Cron trigger based on Indian time

--- cron.js
+++ cron.js
@@ -3,9 +3,9 @@ const cron = require('node-cron');
 const autoTiggerService = require('./service/autoTriggerService');
 const stateDistrictService = require('./service/stateDistrictService');
 
-const task = cron.schedule('0 6,9,12,15,18 * * *', () => {
+const task = cron.schedule('00 6,9,12,15,18 * * *', () => {
     console.log('cronjob started...');
-    autoTiggerService.triggerAlertByHour(new Date().getHours());
+    autoTiggerService.triggerAlertByHour(new Date(new Date().toLocaleString("en-US", {timeZone: 'Asia/kolkata'})).getHours());
 }, {
     scheduled: false,
     timezone: "Asia/kolkata"
