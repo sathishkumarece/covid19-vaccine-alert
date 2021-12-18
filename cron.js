f83ef6576eb46aceaef8905d4245d2ee1955b24c
Sathishkumar NATARAJ <sathishkumarece@gmail.com>
2021-12-18 16:30:50 +0100

    Fix: Improved code quality

--- cron.js
+++ cron.js
@@ -1,7 +1,7 @@
 'use strict';
 const cron = require('node-cron');
 const autoTiggerService = require('./service/autoTriggerService');
-const stateDistrictService = require('./service/stateDistrictService');
+// const stateDistrictService = require('./service/stateDistrictService');
 
 const task = cron.schedule('00 6,9,12,15,18 * * *', () => {
     console.log('cronjob started...');
